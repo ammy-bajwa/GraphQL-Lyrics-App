35e69eac7bf4b5742da6974631c81495a8ab721a
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-01-30 17:41:59 +0500

    implementing graphql query on front end

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,10 @@
 import React,{ Component } from "react";
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
 class SongList extends Component{
     render(){
+        console.log(this.props)
         return (
             <div>
                 SongList
@@ -10,4 +13,12 @@ class SongList extends Component{
     }
 }
 
-export default SongList;
+const query = gql`
+{
+    songs {
+        title
+    }
+}
+`
+
+export default graphql(query)(SongList);
