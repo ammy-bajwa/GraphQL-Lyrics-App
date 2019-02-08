02a1eaa57f53186a3aeeb6c5adf76f59d8333098
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-02-08 21:22:05 +0500

    styling the component a little bit using materialse css

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -4,16 +4,16 @@ import { graphql } from 'react-apollo';
 
 class SongList extends Component {
     renderSongList() {
-        return this.props.data.songs.map(song => <li key={song.id}>{song.title}</li>)
+        return this.props.data.songs.map(song => <li key={song.id} className='collection-item'>{song.title}</li>)
     }
     render() {
         if (this.props.data.loading) {
             return <div>Loading........</div>
         }
         return (
-            <div>
+            <ul className='collection'>
                 {this.renderSongList()}
-            </div>
+            </ul>
         )
     }
 }
