ec928bcf485754969a560adf52049e8c537bdb91
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-02-09 09:11:08 +0500

    Delete a song functionality

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import gql from 'graphql-tag';
 import query from '../queries/fetchSong';
+import delSongMutation from '../queries/deleteSong';
 
 class SongList extends Component {
-    renderSongList() {
-        return this.props.data.songs.map(song => <li key={song.id} className='collection-item'>{song.title}</li>)
+    onSongDelete(id) {
+        this.props.mutate({
+            variables: {
+                id
+            },
+            refetchQueries: [{ query }]
+        }).then((result) => {
+            console.log(result, ' Del success')
+        }).catch((err) => {
+            console.log(err)
+        });
     }
+    renderSongList() {
+        return this.props.data.songs.map(({ id, title }) => <li key={id} className='collection-item'>
+            {title}
+            <i className='material-icons right' onClick={() => this.onSongDelete(id)}>delete</i>
+        </li>)
+    }
+
     render() {
         if (this.props.data.loading) {
             return <div>Loading........</div>
@@ -27,4 +45,7 @@ class SongList extends Component {
     }
 }
 
-export default graphql(query)(SongList);
+
+
+
+export default graphql(delSongMutation)(graphql(query)(SongList));
